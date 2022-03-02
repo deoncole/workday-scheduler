5c3f60582ebe8eff1b7628f088a124fe70d27552
Cole <deoncole77@gmail.com>
2022-03-01 19:18:02 -0500

    text area complete with save icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 // set the element to the p tag in the header and set the current day
 var currentDayEl = document.getElementById("currentDay");
-currentDayEl.innerText = "Today is " + moment().format("dddd, MMMM Do, YYYY, h:mm a");
+var time = $(".timeBlk");
+var row = $(".row");
+var descBlk = $(".textarea");
+currentDayEl.innerText = "Today is " + moment().format("dddd, MMMM Do, YYYY");
+var hourNow = moment().format("k");
+
+$(descBlk).each(function() {
+    if ($(this).data("time") < hourNow){
+        // console.log("less than 11");
+        $(this).addClass("past");
+    } else if ($(this).data("time") > hourNow){
+        console.log("more than 11");
+        $(this).addClass("future");
+    } else {
+        // console.log("is 11");
+        $(this).addClass("present");
+    }
+});
+
