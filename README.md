da49d03e3e17718db4671eb1c47634ecf07e9dd9
Cole <deoncole77@gmail.com>
2022-03-01 23:34:28 -0500

    set and get local storage

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Work Day Scheduler Starter Code
+
+## Scheuduler to keep track of your workday. Enter your schedule in the appropriate time and click the save button. Gray items are in the past, red for right now, and green for upcoming. 
+
+## Link to application: https://deoncole.github.io/workday-scheduler/
+
+## ![App Screenshot](/assets/images/workday-scheduler-index-html.png?raw=true "Workday Scheduler")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,70 @@
-// set the element to the p tag in the header and set the current day
+// set the element to the p tag in the header and set the text to the current day 
 var currentDayEl = document.getElementById("currentDay");
-var time = $(".timeBlk");
-var row = $(".row");
-var descBlk = $(".textarea");
 currentDayEl.innerText = "Today is " + moment().format("dddd, MMMM Do, YYYY");
-var hourNow = moment().format("k");
 
+// set variable to hold the text area
+var descBlk = $(".textarea");
+
+// variable to hold the current hour 
+var hourNow = moment().format("k");
+hourNow = 12;
+// function to reload the page after 30 minutes
+var reload = function() {
+    setTimeout(function(){
+        setTimeout(function(){
+            location.reload(true);
+        }, (60000*30));
+    });
+};
+
+// function to run when the page first loads
+$(document).ready(function(){
+
+    // loop through the textareas and set the values to what is in local storage
+    $(descBlk).each(function (){
+        $(this).val(localStorage.getItem($(this).data("time")));
+    })
+
+    // call the reload function
+    reload();
+
+});
+
+// loop throught the textarea by accessing the data index and comparing it to the current hour. If it's before the current hour assign it the past class, equals the current hour assign it the present class, and if it's after assign it the future class.
 $(descBlk).each(function() {
     if ($(this).data("time") < hourNow){
-        // console.log("less than 11");
         $(this).addClass("past");
+        // disable the textarea and make it readonly
+        $(this).attr("disabled", "readonly")
     } else if ($(this).data("time") > hourNow){
-        console.log("more than 11");
         $(this).addClass("future");
     } else {
-        // console.log("is 11");
-        $(this).addClass("present");
+        $(this).addClass("present"); 
     }
 });
+
+// if the text area is in focus, pluse the save icon to notify the user to save
+$(".textarea").on("focus", function(){
+    $(".saveTxt").addClass("pulse");
+})
+
+// save the inserted text to local storage
+$(".saveTxt").on("click", function(){
+    
+    //remove the pluse from the icon
+    $(".saveTxt").removeClass("pulse");
+    
+    // create local variables to hold the key value pairs
+    var hour = "";
+    var text = "";
+
+    // loop through the text areas and set the key value pairs to local storage
+    $(descBlk).each(function (){
+        hour = $(this).data("time");
+        text = $(this).val().trim();
+        localStorage.setItem(hour, text);
+    });
+    
+});
+
 
